684418b947ee55c45158e98fcd010415f67d2cba
Steve Riesenberg <sriesenberg@vmware.com>
2023-03-27 13:01:39 -0500

    Remove WebSecurityConfigurerAdapter

--- flights-api/src/main/java/com/example/api/SecurityConfiguration.java
+++ flights-api/src/main/java/com/example/api/SecurityConfiguration.java
@@ -1,37 +1,35 @@
 package com.example.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-	@Autowired
-	private ApplicationEventPublisher eventPublisher;
-
-	@Override
-	public void configure(WebSecurity web) {
-		web.requestRejectedHandler((request, response, requestRejectedException) -> {
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer(ApplicationEventPublisher eventPublisher) {
+		return (web) -> web.requestRejectedHandler((request, response, requestRejectedException) -> {
 			// Publish a custom event for RequestRejectedException
 			RequestRejectedEvent event = new RequestRejectedEvent(requestRejectedException);
-			this.eventPublisher.publishEvent(event);
+			eventPublisher.publishEvent(event);
 			// Simply throw exception after publishing event
 			throw requestRejectedException;
 		});
 	}
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http
 			.authorizeRequests((authz) -> authz
@@ -42,6 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			)
 			.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		// @formatter:on
+
+		return http.build();
 	}
 
 	@Bean
